858cf8fd71d96cc925e017d72fcea8755c073795
Boffin <fnamily@mail.ru>
2020-12-25 16:34:21 +0300

    React Pizza Project

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 import store from "./redux/store";
@@ -12,11 +12,11 @@ import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
+    <HashRouter>
       <Provider store={store}>
         <App />
       </Provider>
-    </Router>
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
