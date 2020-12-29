7cd1fb1eeb807336c3292afd76c43e80f3f2d397
Boffin <fnamily@mail.ru>
2020-12-29 16:04:56 +0300

    React Pizza Project

--- src/redux/actions/pizzas.js
+++ src/redux/actions/pizzas.js
@@ -5,7 +5,6 @@ export const fetchPizzas = (sortBy, category) => (dispatch) => {
     axios.get(`/pizzas?${category !== null ? `category=${category}` : ''}&_sort=${sortBy.type}&_order=${sortBy.order}`)
         .then(({ data }) => {
             dispatch(setPizzas(data));
-            console.log(category);
         });
 };
 
